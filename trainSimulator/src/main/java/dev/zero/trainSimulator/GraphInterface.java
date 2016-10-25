d2857f465023b1842d2852f23ce802eca1ecc11b
Priyabrata Palit <priyabrata.palit@radian.biz>
2016-10-25 15:04:06 -0400

    removed comments

--- trainSimulator/src/main/java/dev/zero/trainSimulator/GraphInterface.java
+++ trainSimulator/src/main/java/dev/zero/trainSimulator/GraphInterface.java
@@ -1,4 +1,5 @@
 package dev.zero.trainSimulator;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -16,7 +17,8 @@ import dev.zero.trainSimulator.view.DisplayShowTrain;
 import dev.zero.trainSimulator.view.DisplayStationTimePanel;
 import dev.zero.trainSimulator.view.DisplayTablePane;
 
-public class GraphInterface extends JFrame implements ActionListener{//,ItemListener {
+public class GraphInterface extends JFrame implements ActionListener {// ,ItemListener
+																		// {
 
 	/**
 	 * 
@@ -26,63 +28,26 @@ public class GraphInterface extends JFrame implements ActionListener{//,ItemList
 	private GraphPanel pnl;
 	private JTabbedPane tabpan;
 
-	private JPanel jPanel1, jPanel3, jPanel4, jPanel5, jPanel6,jPanel7,jPanel8,jPanel9 ;
-	int usertype=0;
-	private JPanel refresh_panel =new JPanel();
-	private JPanel logout_panel =new JPanel();
-	//private JPanel radio_panel =new JPanel();
-	
+	private JPanel jPanel1, jPanel3, jPanel4, jPanel5, jPanel6, jPanel7, jPanel8, jPanel9;
+	int usertype = 0;
+	private JPanel refresh_panel = new JPanel();
+	private JPanel logout_panel = new JPanel();
+	// private JPanel radio_panel =new JPanel();
+
 	private Rule columnView;
 	private Rule rowView;
 	private JButton refresh = new JButton("Refresh");
 	private JButton logout = new JButton("Logout");
-	
-	//private JRadioButton opt1 = new JRadioButton("met",false);
-	//private JRadioButton opt2 = new JRadioButton();
-	//private JRadioButton opt3 = new JRadioButton();
-	//private JRadioButton opt4 = new JRadioButton();
-	//private ButtonGroup group = new ButtonGroup();
 
 	GraphInterface(int user) {
-	usertype=user;
-	/*
-	JFrame frm = new JFrame("Scheduling");
-    JProgressBar jProgressBar1 = new JProgressBar();
-    JLabel jLabel1 = new javax.swing.JLabel();
+		usertype = user;
 
-    frm.getContentPane().setLayout(null);
+		try {
+			javax.swing.UIManager.setLookAndFeel("com.birosoft.liquid.LiquidLookAndFeel");
 
-    frm.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-    frm.add(jProgressBar1);
-    jProgressBar1.setBounds(40, 80, 290, 16);
+		} catch (Exception e) {
+		}
 
-    jLabel1.setText("Please Wait while Sscheduling is done...");
-    frm.add(jLabel1);
-    jLabel1.setBounds(50, 40, 290, 14);
-
-    java.awt.Dimension screenSize1 = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-    frm.setBounds((screenSize1.width-395)/2, (screenSize1.height-143)/2, 395, 143);
-    frm.setAlwaysOnTop(true);
-
-
-
-frm.setVisible(true);
-jProgressBar1.setIndeterminate(true);
-*/
-	 try {
-	javax.swing.UIManager.setLookAndFeel("com.birosoft.liquid.LiquidLookAndFeel");
-	//javax.swing.UIManager.setLookAndFeel("com.l2fprod.gui.plaf.skin.SkinLookAndFeel");
-	
-	// com.birosoft.liquid.LiquidLookAndFeel.setLiquidDecorations(true);
-	//.setLiquidDecorations(true);
-	 //com.birosoft.liquid.LiquidLookAndFeel.setLiquidDecorations(true, "mac");
-
-    } catch (Exception e) {}    
-/*try {
-	javax.swing.UIManager.setLookAndFeel("org.freeasinspeech.kdelaf.KdeLAF");
-   } catch (Exception e) {}  */
-   
-		
 		pnl = new GraphPanel();
 		scroller = new JScrollPane(pnl);
 
@@ -95,10 +60,11 @@ jProgressBar1.setIndeterminate(true);
 		jPanel5 = new SimulateTrain(pnl);
 		jPanel6 = new DisplayAddTrain();
 		jPanel7 = new DisplayShowTrain();
-		
+
 		jPanel8 = new DisplayStationTimePanel(pnl);
 		jPanel9 = new DisplayDeleteTrain();
-	//	System.out.println("_____________________________________station name______"+pnl.station_name);
+		// System.out.println("_____________________________________station
+		// name______"+pnl.station_name);
 		columnView = new Rule(Rule.HORIZONTAL, false);
 		rowView = new Rule(Rule.VERTICAL, false);
 		columnView.setVisible(true);
@@ -118,13 +84,10 @@ jProgressBar1.setIndeterminate(true);
 		setTitle("GraphInterface");
 		setForeground(new java.awt.Color(0, 102, 204));
 		setName("GraphInterface");
-		scroller.setBorder(javax.swing.BorderFactory.createLineBorder(
-				new java.awt.Color(102, 0, 102), 3));
-		scroller
-				.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scroller.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(102, 0, 102), 3));
+		scroller.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		scroller.setToolTipText("Railway Map");
-		scroller
-				.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scroller.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
 		pnl.setLayout(null);
 
 		pnl.setPreferredSize(new java.awt.Dimension(900, 800));
@@ -144,26 +107,14 @@ jProgressBar1.setIndeterminate(true);
 		getContentPane().add(refresh_panel);
 		refresh.addActionListener(this);
 		refresh_panel.setBounds(100, 350, 1210, 70);
-		
-		
+
 		logout_panel.add(logout);
 		getContentPane().add(logout_panel);
 		logout.addActionListener(this);
 		logout_panel.setBounds(100, 390, 1210, 70);
-		
-			//group.add(opt1);
-			//group.add(opt2);
-			//opt1.addItemListener(this);
-			//opt2.addItemListener(this);
-			//radio_panel.add(opt1);
-			//radio_panel.add(opt2);
-			//getContentPane().add(radio_panel);
-			//radio_panel.setBounds(100, 430, 1210, 70);
-			
-			
-		// jPanel1.setLayout(null);
 
-		tabpan.addTab("Add New Station", null, jPanel1,"click to add new station");
+
+		tabpan.addTab("Add New Station", null, jPanel1, "click to add new station");
 
 		// jPanel3.setLayout(null);
 
@@ -171,75 +122,54 @@ jProgressBar1.setIndeterminate(true);
 
 		// jPanel4.setLayout(null);
 
-		tabpan.addTab("Move Station", null, jPanel4,"click to move stations on map");
+		tabpan.addTab("Move Station", null, jPanel4, "click to move stations on map");
 
 		// jPanel5.setLayout(null);
 
 		tabpan.addTab("Simulate", null, jPanel5, "click to simulate trains");
 
 		tabpan.addTab("Add New  Train", null, jPanel6, "click to add new trains");
-		
+
 		tabpan.addTab("show Train Time Table", null, jPanel7, "click to show time tables of trains");
 
 		tabpan.addTab("show Station Time Table", null, jPanel8, "click to show time tables of stations ");
-		
+
 		tabpan.addTab("Delete train", null, jPanel9, "click to delete train");
-		
+
 		getContentPane().add(tabpan);
 		tabpan.setBounds(20, 470, 720, 170);
-	
-		
-		
-		java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit()
-				.getScreenSize();
-		setBounds((screenSize.width - 764) / 2, (screenSize.height - 697) / 2,
-				764, 697);
+
+		java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+		setBounds((screenSize.width - 764) / 2, (screenSize.height - 697) / 2, 764, 697);
 		setVisible(true);
-		
-		if(user==2)
-		{
-		tabpan.setEnabledAt(0,false);
-		tabpan.setEnabledAt(1,false);
-		tabpan.setEnabledAt(4,false);
-		tabpan.setEnabledAt(7,false);
-		//tabpan.setEnabledAt(2,false);
-		//tabpan.setEnabledAt(2,false);
+
+		if (user == 2) {
+			tabpan.setEnabledAt(0, false);
+			tabpan.setEnabledAt(1, false);
+			tabpan.setEnabledAt(4, false);
+			tabpan.setEnabledAt(7, false);
+			// tabpan.setEnabledAt(2,false);
+			// tabpan.setEnabledAt(2,false);
 		}
-		
-		//jProgressBar1.setIndeterminate(false);
+
+		// jProgressBar1.setIndeterminate(false);
 	}
-	
-	/*public void itemStateChanged(ItemEvent er) {
-		Object src = er.getSource();
-		boolean on = er.getStateChange() == ItemEvent.SELECTED;
-		if (src == opt1) {
-			if (on == true) {
-			try{
-			javax.swing.UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");} catch(Exception t){}
-							}
-						}
-			if (src == opt2) {
-			if (on == true) {
-			try{
-				javax.swing.UIManager.setLookAndFeel("com.birosoft.liquid.LiquidLookAndFeel");} catch(Exception t){}
-							}
-						}
-		}*/
-	
+
+
+
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == refresh) {
-		this.setVisible(false);
-		this.dispose();
-		GraphInterface gi= new GraphInterface(usertype);
+			this.setVisible(false);
+			this.dispose();
+			GraphInterface gi = new GraphInterface(usertype);
 		}
-		
+
 		if (e.getSource() == logout) {
 			this.setVisible(false);
 			this.dispose();
 			Logon start = new Logon();
 		}
-		
-	
-	}   
+
+	}
 
 }
--- trainSimulator/src/main/java/dev/zero/trainSimulator/Logon.java
+++ trainSimulator/src/main/java/dev/zero/trainSimulator/Logon.java
@@ -1,4 +1,5 @@
 package dev.zero.trainSimulator;
+
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -22,6 +23,7 @@ import javax.swing.JTextField;
 import javax.swing.Timer;
 
 import org.springframework.stereotype.Component;
+
 /**
  * This class is the start of the project.
  * It allows the user to logon to the system in appropriate privilege mode.
@@ -42,36 +44,15 @@ public class Logon extends JFrame implements ActionListener {
 
 	public static void main(String[] args) {
 		Logon start = new Logon();
-	//start.iterate();
-		
-
 	}
 
-	
-	
-	
-	/*
-	 JProgressBar current;
-    JTextArea out;
-    JButton find;
-    Thread runner;
-    int num = 0;
-	JPanel pane = new JPanel();
-	
-	*/
-	
-	 final static int interval = 1000;
-  int i;
-  JLabel label;
-    JProgressBar pb;
-    Timer timer;
-    JButton button;
-	
-	
-	
-	
-	
-	
+	final static int interval = 1000;
+	int i;
+	JLabel label;
+	JProgressBar pb;
+	Timer timer;
+	JButton button;
+
 	private int fl = 1;
 	private JPanel pan = new JPanel();
 	private JLabel lbUser, lbPass;
@@ -79,18 +60,20 @@ public class Logon extends JFrame implements ActionListener {
 	private JPasswordField txtPass;
 	private JButton btnOk, btnCancel;
 	private JRadioButton rad, lbr;
-		String arr_user[]= new String[40];
-		String arr_pass[]= new String[40];
+	String arr_user[] = new String[40];
+	String arr_pass[] = new String[40];
 
 	boolean verify;
 
 	ProgressBar progress;
+
 	public Logon() { // constructor
 		super("Train Scheduling"); // sets the frame name
-		//setIconImage(getToolkit().getImage("Images/Home.gif")); // class JFrame
+		// setIconImage(getToolkit().getImage("Images/Home.gif")); // class
+		// JFrame
 		setSize(275, 300);
 		setResizable(false); // fixing the size of the window
-		progress= new ProgressBar();
+		progress = new ProgressBar();
 		addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent we) {
@@ -116,12 +99,10 @@ public class Logon extends JFrame implements ActionListener {
 		lbr.setBounds(80, 170, 100, 25);
 		lbr.addActionListener(this);
 
-
 		rad.setSelected(true);
 		ButtonGroup bg = new ButtonGroup();
 		bg.add(rad);
 		bg.add(lbr);
-			
 
 		// Setting the Form's Labels.
 		lbUser = new JLabel("Username:");
@@ -158,202 +139,113 @@ public class Logon extends JFrame implements ActionListener {
 		pan.add(btnOk);
 		pan.add(btnCancel);
 
-	/*	current = new JProgressBar(0, 2000);
-		current.setBounds(100, 200, 150, 25);
-		current.setValue(0);
-		current.setStringPainted(true); 
-		
-		pan.add(current); */
-		// Adding Panel to the Form.
-		
-		//pb = new JProgressBar(0, 20);
-		//pb.setBounds(100, 200, 150, 25);
-       // pb.setValue(0);
-       // pb.setStringPainted(true);
-		//pan.add(pb);
 		getContentPane().add(pan);
-		/*
-		timer = new Timer(interval, new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
-        if (i == 20){
-          Toolkit.getDefaultToolkit().beep();
-          System.out.println("here^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-		  timer.stop();
-         // button.setEnabled(true);
-          pb.setValue(0);
-        //  String str = "<html>" + "<font color=\"#FF0000\">" + "<b>" + Downloading completed." + "</b>" + "</font>" + "</html>";
-         // label.setText(str);
-        }
-        i = i + 1;
-                pb.setValue(i);
-            }
-        }); */
-		
-		//setContentPane(pane);
-		// Opening the Database.
+
 		setVisible(true);
 	}
-	
-	
-	
-	/* public void iterate() {
-        while (num < 2000) {
-            current.setValue(num);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) { }
-            num += 95;
-        }
-    }
-  */
-  
-	public int testConnection(int type) //throws SQLException
+
+	public int testConnection(int type) // throws SQLException
 	{
-		String dbURL="jdbc:sqlite:/Users/priyabrata/work/traindb";	
-		Connection con=null;
-		Statement st=null;
-		ResultSet rsDetails=null;
+		String dbURL = "jdbc:sqlite:/Users/priyabrata/work/traindb";
+		Connection con = null;
+		Statement st = null;
+		ResultSet rsDetails = null;
 		String query;
-		boolean flag=false;
-		String table="";
-		if(type==1)
-			{
-				table="account1";
-			}
-		else
-			{
-				table="account";
-			}
-	try
-		{
-		//org.sqlite.JDBC
-		 Class.forName("org.sqlite.JDBC").newInstance();
+		boolean flag = false;
+		String table = "";
+		if (type == 1) {
+			table = "account1";
+		} else {
+			table = "account";
+		}
+		try {
+			// org.sqlite.JDBC
+			Class.forName("org.sqlite.JDBC").newInstance();
 			System.out.println("Driver present");
+		} catch (Exception e) {
+			System.out.println("Can't load driver:");
 		}
-		catch(Exception e)
-		{
-		 System.out.println("Can't load driver:");
-		}
-	try
-		{
-			con=DriverManager.getConnection(dbURL,"","");
+		try {
+			con = DriverManager.getConnection(dbURL, "", "");
 			System.out.println("Connected to database  successfully");
-			st=con.createStatement();
-			query = "select * from "+table;
+			st = con.createStatement();
+			query = "select * from " + table;
 			rsDetails = st.executeQuery(query);
-			
-			int counter=0;
-			while(rsDetails.next())
-			{
-			arr_user[counter]=rsDetails.getString("user_id");//System.out.println(us+us.length());System.out.println(user.getText());
-			arr_pass[counter]=rsDetails.getString("password");//System.out.println(ps+ps.length());System.out.println(password.getText());
-			counter++;
+
+			int counter = 0;
+			while (rsDetails.next()) {
+				arr_user[counter] = rsDetails.getString("user_id");// System.out.println(us+us.length());System.out.println(user.getText());
+				arr_pass[counter] = rsDetails.getString("password");// System.out.println(ps+ps.length());System.out.println(password.getText());
+				counter++;
 			}
 			st.close();
-	 		con.close();
-			for(int k=0;k<counter;k++)
-			{
-			if(txtUser.getText().equals(arr_user[k]) && txtPass.getText().equals(arr_pass[k]))
-			return 1;
+			con.close();
+			for (int k = 0; k < counter; k++) {
+				if (txtUser.getText().equals(arr_user[k]) && txtPass.getText().equals(arr_pass[k]))
+					return 1;
 			}
 			return 0;
-			
+
+		} catch (Exception e) {
+			System.out.println("Errors in reading ");
+			return 0;
 		}
-	catch(Exception e)
-		{
-		 System.out.println("Errors in reading ");
-		 return 0;
-		}
-	
+
 	}
-  
-  
+
 	public void actionPerformed(ActionEvent ae) {
 		Object obj = ae.getSource();
 		if (obj == btnOk) { // If OK Button Pressed.
 			String password = new String(txtPass.getPassword());
 			if (txtUser.getText().equals("")) { // first nested if
-				JOptionPane.showMessageDialog(this,
-						"Provide Username to Logon.");
+				JOptionPane.showMessageDialog(this, "Provide Username to Logon.");
 				txtUser.requestFocus();
 			} else if (password.equals("")) {
 				txtPass.requestFocus();
-				JOptionPane.showMessageDialog(null,
-						"Provide Password to Logon.");
+				JOptionPane.showMessageDialog(null, "Provide Password to Logon.");
 			} else {
 				// String pass; // To Hold the Password.
 				verify = false; // To Confirm Logon.
 
-				if (rad.isSelected()==true) {
+				if (rad.isSelected() == true) {
 					/*
-					 * AdministratorEntity aet = new AdministratorEntity(); aet =
-					 * lm.getData7(txtUser.getText()); user = aet.getuser();
+					 * AdministratorEntity aet = new AdministratorEntity(); aet
+					 * = lm.getData7(txtUser.getText()); user = aet.getuser();
 					 * //Storing UserName. pass = aet.getpassword(); //Storing
 					 * Password.
 					 */
-					int val=testConnection(1);
-					if (val==1) {
+					int val = testConnection(1);
+					if (val == 1) {
 						verify = true;
 
-						
-						
-						// i = 0;
-			         //    timer.start();
-					//	progress.show();
-						
-					
-					/*	ProgressMonitor pm= new ProgressMonitor(Logon.this,
-									"Logging on",
-			                       "hello",
-			                       0,
-			                       1000);
-						pm.setProgress(0);
-						
-					*/ 
-					/*	progress.status(true);
-						PopupFactory factory = PopupFactory.getSharedInstance();
-						Popup popup = factory.getPopup(new JFrame(),progress, 500, 300);
-						popup.show();
-					*/	   
-						  
-
-    						new GraphInterface(1);// Show Main Form.
-						// JOptionPane.showMessageDialog(this,"Correct
-						// Information Provided");
-						//	pm.close();
-    					//	progress.status(false);
-    				//		popup.hide();
-						//progress.hide();
+						new GraphInterface(1);// Show Main Form.
 						
 						setVisible(false); // Hide the Form.
 						dispose(); // Free the System Resources.
 						// }
 					} else {
 						verify = false;
-						JOptionPane.showMessageDialog(this,
-								"Incorrect Information Provided.");
+						JOptionPane.showMessageDialog(this, "Incorrect Information Provided.");
 						txtUser.setText("");
 						txtPass.setText("");
 						txtUser.requestFocus();
 					}
-				} else if (lbr.isSelected()==true) {
+				} else if (lbr.isSelected() == true) {
 					/*
 					 * LibrarianEntity le = new LibrarianEntity(); le =
 					 * lm.getData8(txtUser.getText()); user = le.getusername();
 					 * //Storing UserName. pass = le.getpass(); //Storing
 					 * Password.
 					 */
-				int val=testConnection(2);
-					if (val==1) {
+					int val = testConnection(2);
+					if (val == 1) {
 						verify = true;
 
-					/*	ProgressMonitor pm= new ProgressMonitor(new GraphInterface(2),
-								"Logging on",
-		                       "hello",
-		                       0,
-		                       1000);
-						pm.setProgress(0);
-						*/
+						/*
+						 * ProgressMonitor pm= new ProgressMonitor(new
+						 * GraphInterface(2), "Logging on", "hello", 0, 1000);
+						 * pm.setProgress(0);
+						 */
 						new GraphInterface(2);// Show Main Form.
 						// JOptionPane.showMessageDialog(this,"Correct
 						// Information Provided");
@@ -361,20 +253,19 @@ public class Logon extends JFrame implements ActionListener {
 						dispose(); // Free the System Resources.
 					} else {
 						verify = false;
-						JOptionPane.showMessageDialog(this,
-								"Incorrect Information Provided.");
+						JOptionPane.showMessageDialog(this, "Incorrect Information Provided.");
 						txtUser.setText("");
 						txtPass.setText("");
 						txtUser.requestFocus();
 					}
-				} 
+				}
 			}
 		} else if (obj == btnCancel) { // If Cancel Button Pressed Unload the
 			// From.
 			setVisible(false);
 			dispose();
 			System.exit(0);
-		} 
+		}
 	}
 
 }
